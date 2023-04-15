d5331f96c79922756c8fccfbc5fef0c5481aed33
Iiuliia <yulixan29@gmail.com>
2023-04-15 18:08:57 +0300

    добавлен bottom action bar

--- app/src/main/java/com/example/materialdesign/view/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/materialdesign/view/PictureOfTheDayFragment.kt
@@ -3,13 +3,11 @@ package com.example.materialdesign.view
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.example.materialdesign.MainActivity
 import com.example.materialdesign.R
 import com.example.materialdesign.databinding.FragmentPictureOfTheDayBinding
 import com.example.materialdesign.viewmodel.AppState
@@ -43,17 +41,36 @@ class PictureOfTheDayFragment : Fragment() {
         }
         viewModel.sendRequest()
 
-        binding.chipToday.setOnCloseIconClickListener{
-            Toast.makeText(requireContext(), "Today", Toast.LENGTH_SHORT).show()
+        wikiSearch(binding.inputLine.text.toString())
+
+        setHasOptionsMenu(true)
+        (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_picture_fragment, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+        when(item.itemId){
+            R.id.actionFavorite->{
+
+            }
+            R.id.actionSettings->{
+
+            }
         }
-        binding.chipYesterday.setOnClickListener {
-            Toast.makeText(requireContext(), "Yesterday", Toast.LENGTH_SHORT).show()
-        }
+    }
+
+    private fun wikiSearch(searchText: String){
         binding.wikiInput.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputLine.text.toString()}")
+                data = Uri.parse("https://en.wikipedia.org/wiki/${searchText}")
             })
         }
+
     }
 
     private fun renderData(appState: AppState){
