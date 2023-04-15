cd7be5df040f85ecf770627336e6c00c8caf0ad6
Iiuliia <yulixan29@gmail.com>
2023-04-15 19:24:06 +0300

    добавлены 2 темы, шрифт

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.JupiterTheme)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if(savedInstanceState ==null){
