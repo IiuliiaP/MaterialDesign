c6a0803ed087f1a638f7cb9cddf410d5a0b31962
Iiuliia <yulixan29@gmail.com>
2023-04-25 13:42:59 +0300

    обработка переключения tab item(не работает)

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -8,9 +8,8 @@ import com.example.materialdesign.view.PictureOfTheDayFragment
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        
         setTheme(R.style.JupiterTheme)
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if(savedInstanceState ==null){
--- app/src/main/java/com/example/materialdesign/view/SettingsFragment.kt
+++ app/src/main/java/com/example/materialdesign/view/SettingsFragment.kt
@@ -1,13 +1,17 @@
 package com.example.materialdesign.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.materialdesign.MainActivity
 import com.example.materialdesign.R
 import com.example.materialdesign.databinding.FragmentSettingsBinding
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.BaseOnTabSelectedListener
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
 class SettingsFragment : Fragment() {
     private var _binding: FragmentSettingsBinding? = null
@@ -20,15 +24,43 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
     }
+    private lateinit var parentActivity: MainActivity
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentActivity = activity as MainActivity
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position){
+                    0-> parentActivity.setTheme(R.style.JupiterTheme)
+                    1-> parentActivity.setTheme( R.style.MoonTheme)
+                }
 
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(R.id.imageView, PictureOfTheDayFragment.newInstance()).addToBackStack("").commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                if(tab?.position==0){
+                    parentActivity.setTheme(R.style.MoonTheme)
+
+                }else parentActivity.setTheme(R.style.JupiterTheme)
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                if(tab?.position==0) {
+                    parentActivity.setTheme(R.style.JupiterTheme)
+                }
+
+            }
+        })
 
 
     }
-
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
