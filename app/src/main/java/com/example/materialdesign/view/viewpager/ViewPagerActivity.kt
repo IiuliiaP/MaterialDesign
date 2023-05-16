bb9a721153a861ebd2fae6d71e391abf77728ffe
Iiuliia <yulixan29@gmail.com>
2023-05-16 14:31:01 +0300

    иконки для переключения tab layout

--- app/src/main/java/com/example/materialdesign/view/viewpager/ViewPagerActivity.kt
+++ app/src/main/java/com/example/materialdesign/view/viewpager/ViewPagerActivity.kt
@@ -14,5 +14,15 @@ class ViewPagerActivity : AppCompatActivity() {
         binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
         binding.tabLayoutViewPager.setupWithViewPager(binding.viewPager)
 
+        binding.tabLayoutViewPager.getTabAt(EARTH)?.setIcon(R.drawable.ic_earh)
+        binding.tabLayoutViewPager.getTabAt(MARS)?.setIcon(R.drawable.ic_mars)
+        binding.tabLayoutViewPager.getTabAt(SPACE)?.setIcon(R.drawable.ic_space)
+
+    }
+    companion object{
+        private const val EARTH = 0
+        private const val MARS = 1
+        private const val SPACE = 2
+
     }
 }
