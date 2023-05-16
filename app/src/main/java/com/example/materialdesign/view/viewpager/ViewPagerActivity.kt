ec40d758201b498398d20be1c310c4c597a6e629
Iiuliia <yulixan29@gmail.com>
2023-05-16 17:32:12 +0300

    анимация кнопки

--- app/src/main/java/com/example/materialdesign/view/viewpager/ViewPagerActivity.kt
+++ app/src/main/java/com/example/materialdesign/view/viewpager/ViewPagerActivity.kt
@@ -1,11 +1,14 @@
 package com.example.materialdesign.view.viewpager
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.materialdesign.R
 import com.example.materialdesign.databinding.ActivityViewPagerBinding
 
 class ViewPagerActivity : AppCompatActivity() {
+    var isFlag = false
     private lateinit var binding: ActivityViewPagerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,18 @@ class ViewPagerActivity : AppCompatActivity() {
         binding.tabLayoutViewPager.getTabAt(EARTH)?.setIcon(R.drawable.ic_earh)
         binding.tabLayoutViewPager.getTabAt(MARS)?.setIcon(R.drawable.ic_mars)
         binding.tabLayoutViewPager.getTabAt(SPACE)?.setIcon(R.drawable.ic_space)
+
+        binding.fabViewPagerActivity.setOnClickListener {
+            isFlag = !isFlag
+            if(isFlag){
+                ObjectAnimator.ofFloat(binding.fabViewPagerActivity, View.ROTATION, 0f,720f)
+                    .setDuration(1000L).start()
+
+            }else{
+                ObjectAnimator.ofFloat(binding.fabViewPagerActivity, View.ROTATION, 0f,0f)
+                    .setDuration(1000L).start()
+            }
+        }
 
     }
     companion object{
