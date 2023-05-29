b4ee1a65dcc0f4a4d30cba7f01e8ff56a4cc72db
Iiuliia <yulixan29@gmail.com>
2023-05-29 18:10:55 +0300

    макет recycler activity и его элементов

---
+++ app/src/main/java/com/example/materialdesign/view/recycler/RecyclerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.materialdesign.view.recycler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.materialdesign.R
+
+class RecyclerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler)
+    }
+}
